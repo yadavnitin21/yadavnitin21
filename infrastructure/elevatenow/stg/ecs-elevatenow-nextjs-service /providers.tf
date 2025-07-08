8b65ade71ef0b6546632b5cf321f7845dbe449ef
Nitin Yadav <nitinyadav.ny02@gmail.com>
2025-07-08 13:41:42 +0530

    fix: cleaned up invalid paths and restored valid files

--- infrastructure/elevatenow/stg/ecs-elevatenow-nextjs-service /providers.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region  = "ap-south-1"
-  version = "~> 4"
-  profile = "elevatenow-prod"
-}
--- infrastructure/whealth-ventures/prod/ecs-whealth-segment-service /providers.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region  = "ap-south-1"
-  version = "~> 4"
-  profile = "whealth-ventures-prod"
-}
